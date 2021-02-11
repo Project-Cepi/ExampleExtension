c73c6d8a8cf0b62ec337de7faa8b70fb762f5e52
LeoDog896 <LeoDog896@hotmail.com>
2021-02-11 13:21:26 -0500

    Apply static error

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,3 +64,6 @@ java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
