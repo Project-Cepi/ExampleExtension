c4007260a19460dc7a5def35afa3c0b06ea4bc47
LeoDog896 <LeoDog896@hotmail.com>
2021-06-12 23:19:17 -0400

    Fix repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,14 +20,13 @@ plugins {
 repositories {
     // Use mavenCentral
     mavenCentral()
-    
-    maven(url = "https://repo1.maven.org/maven2/")
-    maven(url = "https://repo.spongepowered.org/maven")
-    maven(url = "https://libraries.minecraft.net")
+
     maven(url = "https://jitpack.io")
-    maven(url = "https://jcenter.bintray.com/")
+    maven(url = "https://repo.spongepowered.org/maven")
+    maven(url = "https://repo.minestom.com/repository/maven-public/")
     maven(url = "https://repo.velocitypowered.com/snapshots/")
 }
+
 
 dependencies {
     // Align versions of all Kotlin components
