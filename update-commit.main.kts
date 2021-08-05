4f9d67db090b9ab288f011a50049ebd8288c69b4
CmdrNorthpaw <thetuxkid@gmail.com>
2021-08-05 21:08:06 +0100

    Use Kord v0.8.0-M3

--- update-commit.main.kts
+++ update-commit.main.kts
@@ -1,6 +1,6 @@
 @file:DependsOn("io.github.microutils:kotlin-logging-jvm:2.0.6")
 @file:DependsOn("org.slf4j:slf4j-simple:1.7.30")
-@file:DependsOn("dev.kord:kord-core:0.7.4")
+@file:DependsOn("dev.kord:kord-core:0.8.0-M3")
 
 import dev.kord.common.Color
 import dev.kord.common.entity.ChannelType
@@ -8,6 +8,7 @@ import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
 import dev.kord.core.behavior.edit
 import dev.kord.core.entity.channel.TextChannel
+import dev.kord.rest.builder.message.modify.embed
 import kotlinx.coroutines.runBlocking
 
 val repoName = args[0].removePrefix("Project-Cepi/")
@@ -20,10 +21,10 @@ runBlocking {
     channel.getMessage(Snowflake(872557344029020190)).edit {
         this.content = ""
 
-        if (embed == null) embed {
+        if (embeds?.isEmpty() == true) embed {
             field(repoName) { "Latest commit: `$commitHash" }
             color = Color(0xFF0000)
-        } else embed?.apply {
+        } else embeds?.get(0)?.apply {
             val field = fields.firstOrNull { it.name == repoName }
             if (field == null) field(repoName) { "Latest commit: `$commitHash" }
             else field.value = "Latest commit: $commitHash"
