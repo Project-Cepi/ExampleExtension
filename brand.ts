6ee96e9399711d3ab13d645bf6a168be8a4644a7
Tristan <LeoDog896@hotmail.com>
2022-02-19 12:40:21 -0500

    Add remote URL

--- brand.ts
+++ brand.ts
@@ -3,12 +3,17 @@ import { ensureFile } from "https://deno.land/std@0.126.0/fs/mod.ts";
 import { prompt, Input } from "https://deno.land/x/cliffy/prompt/mod.ts";
 import { colors } from "https://deno.land/x/cliffy/ansi/colors.ts";
 
+const remoteURL = Deno.run({
+    cmd: ["git", "config", "--get", "remote.origin.url"]
+})?.match(/[\w-]+\/[\w-]+$/)
+
 const generatePackageName = (projectName: string) => (projectName.match(/^(?:(?!Extension).)*/i) as string[])[0].toLowerCase()
 
 const fullDirectoryName = dirname(new URL('', import.meta.url).pathname)
 const directoryName = (fullDirectoryName.match(/\w+(?!\/)$/) as string[])[0]
 
-const projectName: string = await Input.prompt(`Enter the project name (${directoryName})`) || directoryName
+const repoName: string = await Input.prompt(`Enter repository name${remoteURL ? ` (${remoteURL})` : ""}.`) || remoteURL
+const projectName: string = await Input.prompt(`Enter the project name (${directoryName}).`) || directoryName
 const preferredMainClass: string = await Input.prompt(`Enter the preferred class name (${projectName}).`) || projectName
 
 const assumedPackageName = generatePackageName(projectName)
@@ -31,7 +36,7 @@ group=world.cepi.${packageName}
 version=1.0.0`)
 
 await Deno.writeTextFile(paths.readme, `# ${projectName}
-[![license](https://img.shields.io/github/license/Project-Cepi/${projectName}?style=for-the-badge&color=b2204c)](../LICENSE)
+[![license](https://img.shields.io/github/license/${repoName}?style=for-the-badge&color=b2204c)](../LICENSE)
 [![wiki](https://img.shields.io/badge/documentation-wiki-74aad6?style=for-the-badge)](https://project-cepi.github.io/)
 [![discord-banner](https://img.shields.io/discord/706185253441634317?label=discord&style=for-the-badge&color=7289da)](https://discord.cepi.world/8K8WMGV)
 
@@ -39,7 +44,7 @@ ${description}
 
 ## Installation
 
-Download the jar from [Releases](https://github.com/Project-Cepi/${projectName}/releases)
+Download the jar from [Releases](https://github.com/${repoName}/releases)
 OR compile it yourself. Instructions to do so are in Compile header
 
 Drop it into the \`/extensions\` folder.
@@ -49,7 +54,7 @@ Drop it into the \`/extensions\` folder.
 Create a folder, then
 Clone the repository using:
 
-\`git clone https://github.com/Project-Cepi/${projectName}.git\`
+\`git clone https://github.com/${repoName}.git\`
 
 Once it is cloned, make sure you have gradle installed, and run
 
@@ -88,4 +93,5 @@ class ${projectName} : Extension() {
 }
 `)
 
-console.log("🚀", colors.bold.green("Rebranded files"))
+console.log(" ")
+console.log("🚀", colors.bold.green("Rebranded files. Have fun koding!"))
